b3ca20ba68de0176e805c1328351d945b81da1d8
s1081699 <84544811+s1081699@users.noreply.github.com>
2022-05-27 04:31:17 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/csim/luna/car/MainActivity2.java
@@ -0,0 +1,14 @@
+package tw.edu.pu.csim.luna.car;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/tw/edu/pu/csim/luna/car/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package tw.edu.pu.csim.luna.car.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import tw.edu.pu.csim.luna.car.data.LoginDataSource;
+import tw.edu.pu.csim.luna.car.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
